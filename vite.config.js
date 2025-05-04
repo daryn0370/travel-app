9ceb1220f681888b42285ff9d81df44b51b38d88
Daryn <daryn0370@gmail.com>
2025-05-05 00:41:19 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/travel-app/',
   server: {
     proxy: {
       // все запросы к /api/* будут проксироваться на http://localhost:5000/*
